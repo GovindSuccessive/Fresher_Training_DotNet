1876890aa8d18e2031e1c90a4bea79de042fc32b
GovindSuccessive <govind.sharma@successive.tech>
2023-10-19 14:55:21 +0530

    Pending Assignment 2

--- 18-10-23 Assignment 2(Student DB).sql
+++ 18-10-23 Assignment 2(Student DB).sql
@@ -36,82 +36,104 @@ foreign key (StudentID) references tbl_Students(Student_ID),
 foreign key (CourseID) references tbl_Courses(Pk_Course_Id)
 )
 
---Q1. Retrieve the full names of students who are enrolled in more than one course in the "Computer Science" department.
-
-select s.student_firstName,s.student_lastname,count(e.pk_enroll_id)
-from tbl_Students as s
-inner join tbl_Enrollments as E on s.student_id = e.StudentID
-inner join tbl_Courses as C on E.CourseID=C.Pk_Course_id
-group by s.student_firstName,s.student_lastname
-having count( c.departmentid)>1
+--Q1. Retrieve the full names of students who are enrolled in more than one course in the "Front-end" department.
 
 
+ select s.Student_FirstName,s.Student_LastName,d.Dept_Name,count(e.courseId)as Course_Count
+ from tbl_Enrollments as e
+ inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+ inner join tbl_Departments as d on d.Pk_DepartmentID=c.DepartmentID
+ inner join tbl_Students as s on s.Student_id=e.StudentID
+ where d.Dept_Name='front-end'
+ group by s.Student_FirstName,s.Student_LastName,d.Dept_Name
+ having count(e.courseid)>1
 
---Q3. 
+ --Q2 List the names of courses that have at least one student enrolled but have not yet started.
+
+     select distinct c.CourseName
+	 from tbl_Courses as c 
+	 inner join tbl_Enrollments as e on c.Pk_Course_Id=e.CourseID
+	 where e.EnrollmentDate>c.Course_Starting_Date
 
 
+--Q3. Find the students who are enrolled in courses from more than one department.
 
-
-
-
+select s.Student_FirstName,S.Student_LastName,count(distinct c.departmentID) as Department_Count
+from tbl_Enrollments as e
+inner join tbl_Courses as c on e.CourseID = c.Pk_Course_Id
+inner join tbl_Students as s on s.Student_id=e.StudentID
+group by s.Student_FirstName,S.Student_LastName
+having count(distinct c.departmentId)>1
 
 
 --Q4.Retrieve the names of students who are enrolled in all courses offered by the "Engineering" department.
 
-select S.student_firstName,S.Student_lastname
-from tbl_Enrollments as E
-inner join tbl_Courses as C on e.CourseID=c.Pk_course_id
-inner join tbl_Students as S on S.Student_id=E.StudentID
-inner join tbl_Departments as D on D.Pk_DepartmentID=C.DepartmentID
-where D.Dept_Name='Front-end'
-
---Q5
-SELECT d.deptName, c.courseName, COUNT(DISTINCT s.StudentID) AS TotalStudents
-FROM tblDepartments d
-INNER JOIN tblCourses c ON d.deptID = c.deptID
-INNER JOIN tblEnrollments e ON c.courseID = e.courseID
-INNER JOIN tblStudents s ON e.studentID = s.studentID
-GROUP BY d.deptName, c.courseName ORDER By TotalStudents;
-
-
---Q6
-SELECT s.FirstName, s.LastName
-FROM tblStudents s
-INNER JOIN tblEnrollments e ON s.StudentID = e.StudentID
-INNER JOIN tblCourses c ON e.CourseID = c.CourseID
-INNER JOIN tblDepartments d ON c.deptID = d.deptID
-WHERE s.FirstName = d.deptName;
-
-
---Q7
+select s.Student_FirstName,s.Student_LastName,count(e.courseID) as Course_count
+from tbl_Departments as d
+inner join tbl_Courses as c on d.Pk_DepartmentID = c.DepartmentID
+inner join tbl_Enrollments as e on e.CourseID=c.Pk_Course_Id
+inner join tbl_Students as s on s.Student_id=e.StudentID
+where d.Dept_Name='front-end'
+group by s.Student_FirstName,s.Student_LastName
+having count(e.courseId)=2
 
 
 
---Q8
-SELECT s.FirstName, s.LastName
-FROM tblStudents s
-WHERE s.StudentID
-NOT IN 
-(SELECT e.StudentID FROM tblEnrollments e 
-INNER JOIN tblCourses c ON e.CourseID = c.CourseID 
-INNER JOIN tblDepartments d ON c.deptID = d.deptID
-WHERE d.deptName = 'computer');
+--Q5. Calculate the total number of students enrolled in each department for each course.
+
+select distinct c.DepartmentID,d.Dept_Name,c.CourseName,count(e.studentid)
+from tbl_Enrollments as e
+inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+inner join tbl_Departments as d on d.Pk_DepartmentID=c.DepartmentID
+group by c.DepartmentID,c.CourseName,d.Dept_Name
+order by DepartmentID asc
 
 
---Q9
-SELECT  top 1 d.deptName, c.CourseName, MAX(e.enrollDate) AS MostRecentEnrollmentDate
-FROM tblDepartments d
-INNER JOIN tblCourses c ON d.deptID = c.deptID
-INNER JOIN tblEnrollments e ON c.CourseID = e.CourseID
-GROUP BY d.deptName, c.CourseName;
+--Q6. List the students who have the same first name as the department they are enrolled in.
+
+	select s.Student_FirstName,s.Student_LastName
+	from tbl_Enrollments as e
+	inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+	inner join tbl_Students as s on e.StudentID=s.Student_id
+	inner join tbl_Departments as d on d.Pk_DepartmentID=c.DepartmentID
+	where s.Student_FirstName=d.Dept_Name
+
+
+--Q7 Find the department with the highest average number of students enrolled per course.
+
+select  top 1 d.Dept_Name,count(e.CourseID) as Student_count
+from tbl_Enrollments as e
+inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+inner join tbl_Departments as d on d.Pk_DepartmentID= c.DepartmentID
+group by d.Dept_Name
+order by Student_count desc
+
+
+--Q8.Retrieve the students who have not enrolled in any courses offered by the "back-end" department.
+	select * 
+	from tbl_Students
+	where student_id Not in
+	(
+		select  e.StudentID
+		from tbl_Enrollments as e 
+		inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+		inner join tbl_Departments as d on d.Pk_DepartmentID=c.DepartmentID
+		where d.Dept_Name='back-end'
+	)
 
 
 
+--Q9. List the courses with the most recent enrolment date for each department.
+
+select distinct D.Dept_Name,c.CourseName,max(e.EnrollmentDate) as Latest_Enrollment_date
+from tbl_Enrollments as e
+inner join tbl_Courses as c on e.CourseID=c.Pk_Course_Id
+inner join tbl_Departments as d on c.DepartmentID = d.Pk_DepartmentID
+group by D.Dept_Name,c.CourseName
+order by d.Dept_Name
 
 
-
-
-
+--Q10. Calculate the total number of students in the "Engineering" department who have enrolled in more courses than the average number of courses per student in that department.
 
 
 
@@ -119,5 +141,13 @@ SELECT * FROM tbl_Departments
 select * from tbl_Students
 select * from tbl_courses
 select * from tbl_Enrollments
+
+
+
+
+
+
+
+
 
 
