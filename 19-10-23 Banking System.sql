e1b315f27dfaa02e7c4e8e1b9b5a6b146b3067ac
GovindSuccessive <govind.sharma@successive.tech>
2023-10-20 12:36:26 +0530

    New Changes

--- 19-10-23 Banking System.sql
+++ 19-10-23 Banking System.sql
@@ -1,22 +1,37 @@
 
-
 create database db_BankingSystem
 
 use db_bankingSystem
+
+create table tbl_Branch(
+Pk_Branch_id int identity(1001,1) primary key,
+Branch_Name varchar(50),
+Branch_Code int,
+Branch_Address varchar(50),
+Branch_PhNo bigint
+)
+
 
 create table tbl_Customer(
 Pk_Cust_ID int identity(1,1) primary key,
 Cust_Name varchar(50),
 Cust_Address varchar(50),
-Cust_PhoneNo bigint
+Cust_PhoneNo bigint,
+Fk_Branch_Id int,
+Foreign key(Fk_Branch_Id) references tbl_Branch(Pk_Branch_Id)
 )
+
 
 create table tbl_employee(
 Pk_Emp_Id int identity(1,1) primary key,
 Emp_Name varchar(50),
 Emp_Address varchar(50),
-Emp_PhoneNo bigint
+Emp_PhoneNo bigint,
+Fk_Branch_id int,
+foreign key (Fk_Branch_id) references tbl_Branch(Pk_Branch_Id)
 )
+
+select * from tbl_Branch
 
 create table tbl_accounts(
 Pk_Acc_Id int identity(1,1) primary key,
@@ -27,7 +42,9 @@ Date_Of_Opening date,
 Date_Of_Closing date,
 Acc_Status varchar(50),
 FK_Cust_id int,
-Foreign key (FK_Cust_id) references tbl_Customer(Pk_Cust_Id)
+Fk_Branch_Id int,
+Foreign key (FK_Cust_id) references tbl_Customer(Pk_Cust_Id),
+Foreign key(Fk_Branch_Id) references tbl_Branch(Pk_Branch_Id)
 )
 
 create table Transactions(
@@ -35,14 +52,11 @@ Pk_Trans_Id int identity(1,1) primary key,
 Trans_Type varchar(50),
 Amount int,
 Trans_Date date,
-Fk_Emp_Id int,
 FK_Sender_Acc_ID int,
 FK_Receiver_Acc_ID int,
 Foreign key (FK_Sender_Acc_ID) references tbl_Accounts(Pk_Acc_ID),
 Foreign key (FK_Receiver_Acc_ID) references tbl_Accounts(Pk_Acc_ID),
-Foreign key (Fk_Emp_Id) references tbl_employee(Pk_Emp_Id)
 )
-
 
 
 create table tbl_Loan_Details(
